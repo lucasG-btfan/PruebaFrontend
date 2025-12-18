1ae4c25b676d7dd37f7090cd6d44aba20ecaa962
Lucas <lucas.gordillo.3591@gmail.com>
2025-12-18 12:28:12 -0300

    a

--- repositories/client_repository.py
+++ repositories/client_repository.py
@@ -1,4 +1,3 @@
-# repositories/client_repository.py
 from __future__ import annotations
 from sqlalchemy.orm import Session
 from models.client import ClientModel
@@ -11,8 +10,16 @@ class ClientRepository(BaseRepositoryImpl):
     def __init__(self, db: Session):
         super().__init__(ClientModel, ClientSchema, db)
 
+    def get_by_id(self, id: int):
+        """Get client by ID."""
+        return self.db.query(self.model).filter(self.model.id_key == id).first()
+
     def find_by_email(self, email: str):
         """Find client by email."""
-        return self.session.query(self.model).filter(
-            self.model.email == email
-        ).first()
+        return self.db.query(self.model).filter(self.model.email == email).first()
+
+    def search_by_name(self, name: str):
+        """Search clients by name (partial match)."""
+        return self.db.query(self.model).filter(
+            self.model.name.ilike(f"%{name}%")
+        ).all()
